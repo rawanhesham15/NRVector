4865d4d6b0aef4564c6ff75aee9439b226c2a8f4
Nourhan_Mahmoud <101576070+NourhanIdc@users.noreply.github.com>
2022-12-22 17:51:32 +0200

    adding copy constructor & move assignment & pop back & erase with two parameters & capacity & empty & operator  <<

--- NRVectorClass.h
+++ NRVectorClass.h
@@ -43,6 +43,21 @@ public:
         }
     }
 
+    //copy constructor :Initialize with a copy:
+    NRVector (const NRVector& v){
+        capcity = v.capcity;
+        sz = v.sz;
+        vec = new T[capcity];
+        for(int i = 0; i < sz; i++){
+            vec[i] = v.vec[i];
+        }
+    }
+
+    // Delete allocated memory:
+    ~NRVector(){
+        delete[] vec;
+    }
+
     // Copy Assignment Operator:
     NRVector &operator= (const NRVector& v){
         if(this != &v) {
@@ -51,6 +66,25 @@ public:
             delete this->vec;
             capcity = v.capcity;
             sz = v.sz;
+            vec = new T[capcity];
+            for (int i = 0; i < sz; ++i) {
+                vec[i] = v.vec[i];
+            }
+        }
+        else{
+            cout << "Sorry, Can Not Delete My Self" << endl;
+        }
+        return *this;
+    }
+
+    // Move Assignment Operator:
+    NRVector &operator= (const NRVector&& v){
+        if(this != &v) {
+            capcity = 0;
+            sz = 0;
+            delete this->vec;
+            capcity = v.capcity;
+            sz = v.size;
             vec = new T[capcity];
             for (int i = 0; i < sz; ++i) {
                 vec[i] = v.vec[i];
@@ -89,6 +123,17 @@ public:
         return sz;
     }
 
+    // Remove and return last element in vec :
+    T pop_back(){
+        if(sz == 0){
+            throw out_of_range("Vector Is Empty");
+        }
+        sz--;
+        return vec[sz];
+    }
+
+    // Iterators //Supports *, + and ++ operations at least // Can use: typedef T* iterator // Or u can use std::iterator so you can
+    // apply STL algorithms on NRVector
     //iterator begin() // Return an iterator (T*)
     //iterator end() // Return an iterator (T*)
     using iterator = T*;
@@ -100,6 +145,44 @@ public:
     iterator end(){
         return vec + sz;
     }
+
+    // Remove items between iterator 1 <= iterator 2 otherwise do nothing
+    // Throw exception if any iterator outside range
+    void erase(iterator it1, iterator it2){
+        if(it1 < vec || it1 > vec + sz || it2 < vec || it2 > vec + sz){
+            throw out_of_range("Iterator Out Of Range");
+        }
+        if(it1 > it2){
+            throw out_of_range("Iterator 1 Must Be Less Than Iterator 2");
+        }
+        int index1 = it1 - vec;
+        int index2 = it2 - vec;
+        int diff = index2 - index1;
+        for(int i = index1; i < sz - diff; i++){
+            vec[i] = vec[i + diff];
+        }
+        sz -= diff;
+    }
+    //void erase(iterator it1, iterator it2){
+    //        if(it1 < vec || it1 > vec + sz || it2 < vec || it2 > vec + sz){
+    //            throw out_of_range("Iterator Out Of Range");
+    //        }
+    //        if(it1 > it2){
+    //            throw out_of_range("Iterator 1 Must Be Less Than Iterator 2");
+    //        }
+    //        int diff = it2 - it1;
+    //        T* newVec = new T[capcity];
+    //        int j = 0;
+    //        for(int i = 0; i < sz; i++){
+    //            if(vec + i < it1 || vec + i >= it2){
+    //                newVec[j++] = vec[i];
+    //            }
+    //        }
+    //        delete[] vec;
+    //        vec = newVec;
+    //        newVec = nullptr;
+    //        sz -= diff;
+    //    }
 
     // Erase one element of the vector.
     void erase(iterator it){
@@ -148,6 +231,15 @@ public:
         }
     }
 
+
+    //Delete all vector content
+    void clear(){
+        sz = 0;
+        T* newVec = new T[capcity];
+        vec = newVec;
+        newVec = nullptr;
+    }
+
     // Comparison operations:
     //overloading the operator==
     //Return true if all elements in both vectors are equal
@@ -165,10 +257,35 @@ public:
         }
     }
 
+    // Overloading the operator<
+    // Compares item by item and Return true if the first different item in this is < in other:
+    bool operator< (const NRVector<T>& other){
+        if(sz < other.sz){
+            return true;
+        }
+        else if(sz > other.size()){
+            return false;
+        }
+        else {
+            int minSize = min(sz, other.sz);
+            for (int i = 0; i < minSize; i++) {
+                if (vec[i] > other.vec[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     // Capacity operations
     // Return current size of vec:
     int size() const{
         return sz;
+    }
+
+    // Return size of current allocated array:
+    int capacity() const{
+        return capcity;
     }
 
     // Relocate to bigger space:
@@ -184,5 +301,18 @@ public:
         return capcity;
     }
 
+    // Return true if size is 0:
+    bool empty(){
+        if (sz == 0){
+            return true;
+        }
+    }
 
+    // Friends :overloading the operator << :
+    friend ostream& operator << (ostream& out, NRVector<T> v){
+        for (int i = 0; i < v.sz; ++i) {
+            out << v.vec[i] << " ";
+        }
+        return out;
+    }
 };
